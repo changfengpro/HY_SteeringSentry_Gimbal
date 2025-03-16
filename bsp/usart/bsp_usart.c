f8399f7ee98dc091977dc2fa4e39541af495f51f
changfengpro <2728019272@qq.com>
2025-03-16 17:23:14 +0800

    适应导航的抽象通讯

--- bsp/usart/bsp_usart.c
+++ bsp/usart/bsp_usart.c
@@ -108,7 +108,7 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
             if (usart_instance[i]->module_callback != NULL)
             {
                 usart_instance[i]->module_callback();
-                memset(usart_instance[i]->recv_buff, 0, Size); // 接收结束后清空buffer,对于变长数据是必要的
+                // memset(usart_instance[i]->recv_buff, 0, Size); // 接收结束后清空buffer,对于变长数据是必要的
             }
             HAL_UARTEx_ReceiveToIdle_DMA(usart_instance[i]->usart_handle, usart_instance[i]->recv_buff, usart_instance[i]->recv_buff_size);
             __HAL_DMA_DISABLE_IT(usart_instance[i]->usart_handle->hdmarx, DMA_IT_HT);
--- modules/referee_transport/referee_transport.c
+++ modules/referee_transport/referee_transport.c
@@ -5,7 +5,7 @@
  * @version: 
  * @Date: 2025-03-13 06:34:23
  * @LastEditors:  
- * @LastEditTime: 2025-03-16 01:12:31
+ * @LastEditTime: 2025-03-16 17:17:08
  */
 #include "referee_transport.h"
 
@@ -16,7 +16,7 @@
 static referee_info_t referee_data;
 static uint8_t referee_init_flag;
 static uint8_t game_state;
-
+uint8_t test_buff[128];
 static USARTInstance *referee_data_usart_instance;  // 裁判系统数据转发串口实例
 static DaemonInstance *referee_data_daemo_instance; // 裁判系统数据转发进程守护实例
 
@@ -55,7 +55,12 @@ static void RefereeDataRxCallback()
     DaemonReload(referee_data_daemo_instance);   //先喂狗
     RefereeDataParse(referee_data_usart_instance->recv_buff);
     game_state = referee_data.GameState.game_progress;
-    HAL_UART_Transmit_DMA(&huart7, game_state, sizeof(game_state));
+   
+    for(int i = 0; i < 125; i++)
+    {
+        test_buff[i] = game_state;
+    }
+    HAL_UART_Transmit_DMA(&huart7, test_buff, sizeof(test_buff));
 }
 
 /**
